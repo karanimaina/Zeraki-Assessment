283ac43f8f77b18fc88d0b149568c4cd21fc8545
karanimaina <karani.maina2010@gmail.com>
2023-02-16 13:49:48 +0300

    add Getter,Setter and Constructrs for the Course class

--- src/main/java/com/example/zerakiassessment/model/Course.java
+++ src/main/java/com/example/zerakiassessment/model/Course.java
@@ -1,4 +1,15 @@
 package com.example.zerakiassessment.model;
 
-public class Course {
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Course  {
 }
