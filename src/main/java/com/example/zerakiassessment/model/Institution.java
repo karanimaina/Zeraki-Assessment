e8065d1f14cfae4bece135b3baa12e5992f466a4
karanimaina <karani.maina2010@gmail.com>
2023-02-16 22:09:49 +0300

    extend base Entity

--- src/main/java/com/example/zerakiassessment/model/Institution.java
+++ src/main/java/com/example/zerakiassessment/model/Institution.java
@@ -16,8 +16,7 @@ import java.util.List;
 @AllArgsConstructor
 @SQLDelete(sql="UPDATE tb_institution SET soft_delete=true where id=?")
 @Where(clause = "soft_delete = false")
-
-public class University {
+public class Institution extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
