5614ec1ca1606927d4c6a4fa64284385759debfc
karanimaina <karani.maina2010@gmail.com>
2023-02-16 13:56:55 +0300

    setup the firstName , lastname,course and university

--- src/main/java/com/example/zerakiassessment/model/Student.java
+++ src/main/java/com/example/zerakiassessment/model/Student.java
@@ -1,4 +1,19 @@
 package com.example.zerakiassessment.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Student {
+    private  String firstName;
+    private String lastName;
+    private Course course;
+    private University university;
 }
