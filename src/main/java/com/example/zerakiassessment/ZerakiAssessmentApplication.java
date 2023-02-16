be7d72413c6c44a929991ebb6507e277542e7368
karanimaina <karani.maina2010@gmail.com>
2023-02-16 13:22:39 +0300

    first commit

---
+++ src/main/java/com/example/zerakiassessment/ZerakiAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.zerakiassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerakiAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerakiAssessmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zerakiassessment/ZerakiAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zerakiassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerakiAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
