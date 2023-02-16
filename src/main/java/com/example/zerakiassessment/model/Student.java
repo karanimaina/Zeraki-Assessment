03f5a6615f9963c59ea840fb70ba90efb6cfb1c7
karanimaina <karani.maina2010@gmail.com>
2023-02-16 22:01:57 +0300

    add entity and builder class

--- src/main/java/com/example/zerakiassessment/model/Student.java
+++ src/main/java/com/example/zerakiassessment/model/Student.java
@@ -1,17 +1,14 @@
 package com.example.zerakiassessment.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Table(name = "tb_student")
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
+@Builder
+
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
