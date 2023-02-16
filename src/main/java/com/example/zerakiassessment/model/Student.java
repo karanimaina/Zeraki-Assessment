d33b788bff78a1019450e0e24ce2260cdc60ee38
karanimaina <karani.maina2010@gmail.com>
2023-02-16 22:03:40 +0300

    add sql delete and a wgere clause

--- src/main/java/com/example/zerakiassessment/model/Student.java
+++ src/main/java/com/example/zerakiassessment/model/Student.java
@@ -2,6 +2,8 @@ package com.example.zerakiassessment.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Table(name = "tb_student")
 @Getter
@@ -10,6 +12,8 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@SQLDelete(sql="UPDATE tb_student SET soft_delete=true where id=?")
+@Where(clause = "soft_delete = false")
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
