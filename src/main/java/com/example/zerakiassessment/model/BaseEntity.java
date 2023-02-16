c2ec56cf0b9233725e381cbde2d928c6a1f90e7f
karanimaina <karani.maina2010@gmail.com>
2023-02-16 21:51:29 +0300

    add mapped superclass

--- src/main/java/com/example/zerakiassessment/model/BaseEntity.java
+++ src/main/java/com/example/zerakiassessment/model/BaseEntity.java
@@ -1,4 +1,11 @@
 package com.example.zerakiassessment.model;
 
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
 public class BaseEntity {
 }
