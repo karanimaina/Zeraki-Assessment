c52244ddc7b634f3d66b8983007375dc0c0f17e2
karanimaina <karani.maina2010@gmail.com>
2023-02-16 13:52:23 +0300

    add University class and student class

--- src/main/java/com/example/zerakiassessment/model/Course.java
+++ src/main/java/com/example/zerakiassessment/model/Course.java
@@ -1,6 +1,9 @@
 package com.example.zerakiassessment.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,4 +15,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Course  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private University University;
+    private Student student;
 }
