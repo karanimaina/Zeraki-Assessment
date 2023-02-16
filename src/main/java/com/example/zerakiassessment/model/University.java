c432a5083520cee971eef00a8539d16206210c66
karanimaina <karani.maina2010@gmail.com>
2023-02-16 22:07:25 +0300

    set the entity  table

--- src/main/java/com/example/zerakiassessment/model/University.java
+++ src/main/java/com/example/zerakiassessment/model/University.java
@@ -2,12 +2,21 @@ package com.example.zerakiassessment.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import java.util.List;
 
 @Table(name = "tb_institution")
 @Getter
 @Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@SQLDelete(sql="UPDATE tb_institution SET soft_delete=true where id=?")
+@Where(clause = "soft_delete = false")
+
 public class University {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
