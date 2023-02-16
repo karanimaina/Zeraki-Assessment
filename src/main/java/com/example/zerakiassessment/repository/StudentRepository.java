55ca9b555b0872cce1c8ee7e31d9f7df63b02639
karanimaina <karani.maina2010@gmail.com>
2023-02-16 14:21:30 +0300

    add university Repository

--- src/main/java/com/example/zerakiassessment/repository/StudentRepository.java
+++ src/main/java/com/example/zerakiassessment/repository/StudentRepository.java
@@ -1,4 +1,7 @@
 package com.example.zerakiassessment.repository;
 
-public interface StudentRepository {
+import com.example.zerakiassessment.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student,Long> {
 }
---
+++ src/main/java/com/example/zerakiassessment/repository/UniversityRepository.java
@@ -0,0 +1,7 @@
+package com.example.zerakiassessment.repository;
+
+import com.example.zerakiassessment.model.University;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public  interface UniversityRepository extends JpaRepository<University,Long> {
+}
