ac14e97a8907681cd10fdecbc1a288658eeb9fb6
karanimaina <karani.maina2010@gmail.com>
2023-02-16 13:37:58 +0300

    add Getter

--- src/main/java/com/example/zerakiassessment/model/University.java
+++ src/main/java/com/example/zerakiassessment/model/University.java
@@ -2,11 +2,13 @@ package com.example.zerakiassessment.model;
 
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class University {
 
 }
