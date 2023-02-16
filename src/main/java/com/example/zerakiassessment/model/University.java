e4e90374207290f1c171928fb17df39d2bf2f3be
karanimaina <karani.maina2010@gmail.com>
2023-02-16 14:00:29 +0300

    add a one tomany  to university

--- src/main/java/com/example/zerakiassessment/model/University.java
+++ src/main/java/com/example/zerakiassessment/model/University.java
@@ -1,10 +1,9 @@
 package com.example.zerakiassessment.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -16,7 +15,8 @@ public class University {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
-    private Course courses;
+    @OneToMany
+    private List<Course> courses;
     private Student student;
 
 
